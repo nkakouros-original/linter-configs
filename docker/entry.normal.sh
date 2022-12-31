49b6eb16acda35a072c49dce5ad7510cac2327e1
Nikolaos Kakouros <nkak@kth.se>
2022-12-31 13:52:29 +0000

    wip

--- docker/entry.normal.sh
+++ docker/entry.normal.sh
@@ -5,8 +5,8 @@ source /linter-configs/vendor/python/bin/activate
 mkdir -p ~/.ssh
 ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts 2>/dev/null
 
-cp -r /linter-configs/configs/* /repo/
-cp -r /linter-configs/configs/.* /repo/
+cp -nr /linter-configs/configs/* /repo/
+cp -rn /linter-configs/configs/.* /repo/
 export PATH=${PATH}:`go env GOPATH`/bin
 
 if [[ "$#" -gt 0 ]]; then
